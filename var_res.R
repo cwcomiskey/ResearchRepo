12f5b347f1fd0c0cb6ff59f0cca469047217bfc9
Chris Comiskey <cwcomiskey@gmail.com>
2017-06-08 14:17:23 -0700

    Add files via upload

--- var_res.R
+++ var_res.R
@@ -1,42 +1,26 @@
 # Variable-resolution heat maps
 
 # Dependencies, will be in DESCRIPTION file ========== #
-library("ggplot2")
-library(fields)
-library("dplyr")
-library("reshape2")
+# library("ggplot2")
+# library(fields)
+# library("dplyr")
+# library("reshape2")
 
 hitter <- read.csv("~/Desktop/ResearchRepo/Data/hitter.csv") # for DATA file
 cutoff<- 200 # Function argument
 
 # Loop Zero =====
 
-# GrZero <- summarise(hitter,
-#                     min.x = min(px),
-#                     max.x = max(px), # x bounds
-#                     min.y = min(pz),
-#                     max.y = max(pz), # y bounds
-#                     Hitting = mean(hit),     # BA
-#                     Count = dim(hitter)[1],  # Count
-#                     x = (max(px)+min(px))/2, # x center
-#                     y = (max(pz)+min(pz))/2) # y center
+# GrZero <- summarise(hitter,min.x = min(px),max.x = max(px), min.y = min(pz),max.y = max(pz), Hitting = mean(hit), Count = dim(hitter)[1], x = (max(px)+min(px))/2, y = (max(pz)+min(pz))/2)
 
-# ABCE <- with(GrZero,
-#              cbind.data.frame(
-#                px = x, pz = y,
-#                Hitting = round(Hitting, 2),
-#                Count,
-#                widths = max.x - min.x,
-#                heights = max.y - min.y
-#                )
-#              )
+# ABCE <- with(GrZero,cbind.data.frame(px = x, pz = y,Hitting = round(Hitting, 2),Count,widths = max.x - min.x,heights = max.y - min.y))
 
 ABCE <- with(hitter,
              cbind.data.frame(
-               px = (max(px)+min(px))/2,
-               pz = (max(pz)+min(pz))/2,
-               Hitting = round(mean(hit), 3),
-               Count = dim(hitter)[1],
+               px = (max(px)+min(px))/2,       # center
+               pz = (max(pz)+min(pz))/2,       # center
+               Hitting = round(mean(hit), 3),  # mean
+               Count = dim(hitter)[1],         # obs
                widths = max(px) - min(px),
                heights = max(pz) - min(pz)
              )
@@ -49,7 +33,8 @@ mapit(ABCE)
 # ABCE <-  with(GrZero, mutate(ABCE, width = max.x - min.x, height = max.y - min.y))
 
 # as.image(...)
-gridder <- with(hitter, as.image(hit, cbind.data.frame(px, pz), nx = 2, ny = 2))
+gridder <- with(hitter,
+                as.image(hit, cbind.data.frame(px, pz), nx = 2, ny = 2))
 
 # Manually add bounds and centers
 # gridder$xbb <- with(GrZero, seq(min.x, max.x, , 5))[c(1,3,5)] # x box boundaries
@@ -71,18 +56,20 @@ ABCE <- with(gridder, cbind(
   expand.grid(px = x, pz = y),
   Hitting = as.vector(z),
   Count = as.vector(weights),
-  widths = rep(bw, 2),
-  heights = rep(bh, 2)
+  widths = rep(bw, 4),
+  heights = rep(bh, 4)
                             )
              )
 
 mapit(ABCE, widths, heights)
 
 # Loop 2 (16) ===================================
-
+counter <- 0
 for(i in 1:2){
   for(j in 1:2){
-    if(gridder$weights[i,j] > cutoff){
+    if(gridder$weights[i,j] > cutoff){ # Why gridder, not ABCE?
+
+      counter <- counter + 1
 
       # Filter original data
       Box_ij <- with(gridder, filter(hitter,
@@ -103,11 +90,11 @@ for(i in 1:2){
 
       ABCE_Box_ij <- with(gridder_ij, cbind(expand.grid(px = x, pz = y),
                                             Hitting = as.vector(z),
-                                            Count = as.vector(weights)
+                                            Count = as.vector(weights),
+                                            widths = rep(gridder$bw/2, 2),
+                                            heights = rep(gridder$bh/2, 2)
                                             )
                           )
-
-      # names(ABCE_Box_ij) <- c("px", "pz", "Hitting", "Count")
 
       # ABCE: Remove old box (i,j), add new boxes ======== #
       ABCE <- with(gridder, filter(ABCE, !(px == x[i] & pz == y[j]))) # remove
@@ -117,25 +104,26 @@ for(i in 1:2){
   }
 }
 
-# Solve: 2*2 - x + 4*x = dim(ABCE)[1]
-sdb <- (dim(ABCE)[1] - 4)/3 # number of subdivided boxes
-
-# New box dimensions for ggplot() and geom_tile()
-widths <- with(gridder, c(rep(bw, 4 - sdb), rep(bw/2, 4*sdb)))
-heights <- with(gridder, c(rep(bh, 4 - sdb), rep(bh/2, 4*sdb)))
-ABCE <- cbind(ABCE, heights, widths)
+# # # Solve: 2*2 - x + 4*x = dim(ABCE)[1]
+# sdb <- (dim(ABCE)[1] - 4)/3 # number of subdivided boxes; replace with "counter"
+#
+# # # New box dimensions for ggplot() and geom_tile()
+# widths <- with(gridder, c(rep(bw, 4 - counter), rep(bw/2, 4*counter)))
+# heights <- with(gridder, c(rep(bh, 4 - counter), rep(bh/2, 4*counter)))
+# ABCE <- cbind(ABCE, heights, widths)
 
 mapit(ABCE)
 
 # Loop 3 (64) ========================================
 
 dim(ABCE)[1] # [1] 16
-
+counter <- 0
 LoopData <- data.frame()
 
 for(r in 1:dim(ABCE)[1]){ # Iterate through rows (boxes) of ABCE
 
   if(ABCE$Count[r] > cutoff){
+    counter <- counter + 1
 
     # Divided Box Bounds Lower/Upper
     Dbbl.x <- ABCE[r,"px"] - ABCE[r, "widths"]/2
@@ -161,38 +149,29 @@ for(r in 1:dim(ABCE)[1]){ # Iterate through rows (boxes) of ABCE
                       )
     )
 
-    # For ABCE, for book keeping, for plotting
-    heights <- rep(ABCE[r,5]/2, 4)
-    widths <- rep(ABCE[r,6]/2, 4)
+    # # For ABCE, for book keeping, for plotting
+    # heights <- rep(ABCE[r,"heights"]/2, 4)
+    # widths <- rep(ABCE[r,"widths"]/2, 4)
 
     # Important-stuff data frame
     ABCE_Box_r <- with(gridder_r,
-                       cbind(expand.grid(x,y),
-                             as.vector(z),
-                             as.vector(weights),
-                             heights,
-                             widths))
-
-    names(ABCE_Box_r) <- c("px", "pz", "Hitting", "Count", "heights", "widths")
+                       cbind(expand.grid(px = x, pz = y),
+                             Hitting = as.vector(z),
+                             Count = as.vector(weights),
+                             heights = rep(ABCE[r,"heights"]/2, 4),
+                             widths = rep(ABCE[r,"widths"]/2, 4)))
 
     LoopData <- rbind.data.frame(LoopData, ABCE_Box_r)
   }
 }
 
-# Boxes subdivided
-sdb <- sum(as.numeric(ABCE$Count > cutoff))
 # Remove subdivided, combine with new
 ABCE <- rbind.data.frame(filter(ABCE, Count <= cutoff), LoopData)
 
-ggplot(ABCE, aes(px, pz, fill=Hitting)) +
-  with(ABCE, geom_tile(width = widths, height = heights)) +
-  coord_equal() +
-  scale_fill_distiller(palette = "Spectral", trans="reverse") +
-  geom_text(aes(fill = Hitting, # print counts
-                label = Count), size = 3.5)
+mapit(ABCE)
 
 # Loop 4 (256) ======================================
-
+counter <- 0
 dim(ABCE)[1] # [1] 49 (of 64 possible)
 
 LoopData <- data.frame()
@@ -200,6 +179,7 @@ LoopData <- data.frame()
 for(r in 1:dim(ABCE)[1]){ # Iterate through rows (boxes) of ABCE
 
   if(ABCE$Count[r] > cutoff){
+    counter <- counter + 1
 
     # Divided Box Bounds Lower/Upper
     Dbbl.x <- ABCE[r,"px"] - ABCE[r, "widths"]/2
@@ -224,44 +204,37 @@ for(r in 1:dim(ABCE)[1]){ # Iterate through rows (boxes) of ABCE
                                grid=list(x = Dbc.x, y=Dbc.y)
                       ))
 
-    # For ABCE, for book keeping, for plotting
-    heights <- rep(ABCE[r,5]/2, 4)
-    widths <- rep(ABCE[r,6]/2, 4)
+    # # For ABCE, for book keeping, for plotting
+    # heights <- rep(ABCE[r,5]/2, 4)
+    # widths <- rep(ABCE[r,6]/2, 4)
 
     # Important-stuff data frame
     ABCE_Box_r <- with(gridder_r,
-                       cbind(expand.grid(x,y),
-                             as.vector(z),
-                             as.vector(weights),
-                             heights,
-                             widths))
-
-    names(ABCE_Box_r) <- c("px", "pz", "Hitting", "Count", "heights", "widths")
+                       cbind(expand.grid(px = x, pz = y),
+                             Hitting = as.vector(z),
+                             Count = as.vector(weights),
+                             heights = rep(ABCE[r,"heights"]/2, 4),
+                             widths = rep(ABCE[r,"widths"]/2, 4)))
 
     LoopData <- rbind.data.frame(LoopData, ABCE_Box_r)
   }
 }
 
 sum(as.numeric(ABCE$Count > cutoff)) # number boxes subdivided
+
 # Remove subdivided, combine with new
 ABCE <- rbind.data.frame(filter(ABCE, Count <= cutoff), LoopData)
 
-ggplot(ABCE, aes(px, pz, fill=Hitting)) +
-  with(ABCE, geom_tile(width = widths, height = heights)) +
-  coord_equal() +
-  scale_fill_distiller(palette = "Spectral", trans="reverse") +
-  geom_text(aes(fill = Hitting, # print counts
-                label = Count), size = 3.5)
+mapit(ABCE)
 
 # Loop 5 (4^5) ===========================================
-
-dim(ABCE)[1]
-
+counter <- 0
 LoopData <- data.frame()
 
 for(r in 1:dim(ABCE)[1]){ # Iterate through rows (boxes) of ABCE
 
   if(ABCE$Count[r] > cutoff){
+    counter <- counter + 1
 
     # Divided Box Bounds Lower/Upper
     Dbbl.x <- ABCE[r,"px"] - ABCE[r, "widths"]/2
@@ -285,32 +258,24 @@ for(r in 1:dim(ABCE)[1]){ # Iterate through rows (boxes) of ABCE
                                nx=2, ny=2,
                                grid=list(x = Dbc.x, y=Dbc.y)
                       ))
+#
+#     # For ABCE, for book keeping, for plotting
+#     heights <- rep(ABCE[r,5]/2, 4)
+#     widths <- rep(ABCE[r,6]/2, 4)
 
-    # For ABCE, for book keeping, for plotting
-    heights <- rep(ABCE[r,5]/2, 4)
-    widths <- rep(ABCE[r,6]/2, 4)
-
-    # Important-stuff data frame
+    # Current box important-stuff data frame
     ABCE_Box_r <- with(gridder_r,
-                       cbind(expand.grid(x,y),
-                             as.vector(z),
-                             as.vector(weights),
-                             heights,
-                             widths))
-
-    names(ABCE_Box_r) <- c("px", "pz", "Hitting", "Count", "heights", "widths")
+                       cbind(expand.grid(px = x, pz = y),
+                             Hitting = as.vector(z),
+                             Count = as.vector(weights),
+                             heights = rep(ABCE[r,"heights"]/2, 4),
+                             widths = rep(ABCE[r,"widths"]/2, 4)))
 
     LoopData <- rbind.data.frame(LoopData, ABCE_Box_r)
   }
 }
 
-sum(as.numeric(ABCE$Count > cutoff)) # number boxes subdivided
 # Remove subdivided, combine with new
 ABCE <- rbind.data.frame(filter(ABCE, Count <= cutoff), LoopData)
 
-ggplot(ABCE, aes(px, pz, fill=Hitting)) +
-  with(ABCE, geom_tile(width = widths, height = heights)) +
-  coord_equal() +
-  scale_fill_distiller(palette = "Spectral", trans="reverse") +
-  geom_text(aes(fill = Hitting, # print counts
-                label = Count), size = 3.5)
+mapit(ABCE)
