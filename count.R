f117a0779ea036f202e7f0b7514dea5e2550c792
Chris Comiskey <cwcomiskey@gmail.com>
2017-06-14 16:17:15 -0700

    Add files via upload

--- count.R
+++ count.R
@@ -14,62 +14,61 @@ library("dplyr")
 library("reshape2")
 library("dbplyr")
 
+# *ab_id* links 'atbat' and 'pitches' ------------------ #
+
 db <- src_mysql("Baseball", 
                 create = FALSE, user = "root", password = "Koufax32")
 
-atbat <- tbl(db, "atbats") # representation
-tbl_vars(atbat)
+atbat <- tbl(db, "atbats"); tbl_vars(atbat)
+pitches <- tbl(db, "pitches"); tbl_vars(pitches) 
 
-# atbat2 <- tbl(db, "atbats") %>% select(des, event) %>% collect(n = Inf)
+sw <- tbl(db, "pitches") %>% 
+  filter(des %in% c("Foul", "Foul (Runner Going)",  "Foul Tip", "In play, no out", "In play, out(s)", "Swinging Strike", "Swinging Strike (Blocked)")) %>% 
+  select(px, pz, des, ab_id, pitch_id, id, type); tbl_vars(sw)
 
 
-pitches <- tbl(db, "pitches") # representation
-tbl_vars(pitches) 
+atbats <- tbl(db, "atbats") %>% 
+  select(ab_id, stand, batter, ball, strike, event) # tbl_vars(atbats)
 
-# pitches2 <- tbl(db, "pitches") %>% select(des)
-# tbl_vars(pitches2)
-# pitches2 <- collect(pitches2, n = Inf)
 
-sw <- tbl(db, "pitches") %>%
-  filter(des %in% c("Foul", "Foul (Runner Going)", 
-                    "Foul Tip", "In play, no out", 
-                    "In play, out(s)", "Swinging Strike",
-                    "Swinging Strike (Blocked)")) %>% 
-  select(px, pz, des, ab_id, pitch_id, id, type) 
-tbl_vars(sw)
-
-# *ab_id* links 'atbat' and 'pitches'
-
-atbats <- tbl(db, "atbats") %>% select(ab_id, stand, batter, ball, strike, event)
-tbl_vars(atbats)
-
-# collect() 
 pitches <- left_join(sw, atbats) %>% 
   collect(n = Inf) # REPRESENTATION becomes DATA; THE KEY STEP
 
-comeonbaby <- select(pitches, des, event) %>% filter(event == "Home Run")
+# Verify that HRs are included in "In play, no out" ---------- #
+comeonbaby <- select(pitches, des, event) %>% 
+  filter(event == "Home Run")
 table(comeonbaby)
 
-length(unique(pitches$batter)) # [1] 3369 hitters
-table(pitches$batter)
-summary(as.numeric(table(pitches$batter)))
-
-# Missing-ness --------- #
+# Missing-ness --------------------------- #
 sum(is.na(pitches$pz))
 pitches <- filter(pitches, px != "NA", strike != "NA", ball != "NA", strike != "", ball != "", ball != 4, strike != 3)
 
+# Tabulate ball-strike counts ------------ #
 table(pitches$strike, pitches$ball)
 
+# Count totals for RH hitters --------------------------- #
 rh_count <- pitches %>% filter(stand == "R") %>%
   mutate(hit = as.numeric(des == "In play, no out")) %>%
-  filter(abs(px) < 4) %>% select(px, pz, ball, strike, hit)
+  filter(abs(px) < 4) %>% select(ball, strike, hit)
 
-# Three ball counts only
-threeball <- pitches %>% filter(ball == 3) %>% 
+rhbycount <- group_by(rh_count, ball, strike) %>% 
+  summarise(p = round(mean(hit), 4))
+
+rhbycount <- spread(rhbycount, key = ball, value = p)
+
+names(rhbycount) <- c("Strikes", "0 Balls", "1 Ball", "2 Balls", "3 Balls")
+
+rhbycount
+
+# Three ball counts ----------------- #
+threeball <- pitches %>% 
+  filter(ball == 3) %>% 
   mutate(hit = as.numeric(des == "In play, no out"))
 
-# BATTING AVERAGE three ball counts (in play)
-three_ball_batavg <- filter(threeball, des %in% c("In play, no out", "In play, out(s)")) %>% mutate(hit = as.numeric(des == "In play, no out"))
+# BA affecting three ball counts (ball in play) --------------- #
+three_ball_batavg <- filter(
+  threeball, des %in% c("In play, no out", "In play, out(s)")) %>% 
+  mutate(hit = as.numeric(des == "In play, no out"))
 
 group_by(three_ball_batavg, ball, strike) %>% 
   summarise(p = round(mean(hit), 3), n = n())
@@ -80,7 +79,7 @@ cba <- group_by(rh_count, ball, strike) %>%
   summarise(p = round(mean(hit), 3), n = n())
 
 library(tidyr)
-# Count success probabilities
+# Count success probabilities table ------------------- #
 csp <- as.data.frame(spread(select(cba, ball, strike, p), key = ball, value = p))
 names(csp) <- c("Strikes", "0 Balls", "1 Ball", "2 Balls", "3 Balls")
 
