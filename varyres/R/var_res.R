911fda0351b4d73ebf5b9a8cb899a213d69907f6
Chris Comiskey <cwcomiskey@gmail.com>
2017-06-21 14:40:56 -0700

    Add files via upload

--- varyres/R/var_res.R
+++ varyres/R/var_res.R
@@ -2,23 +2,29 @@
 #'
 #' This function creates variable resolution heat maps accroding to a stopping rule
 #' @param cutoff Box subdivisions cease when a box sample size drops below the cutoff
+#' @param px vector of location horizontal components
+#' @param pz vector of location vertical components
+#' @param hit Bernoulli responses associated with (px, pz) location
+#'
 #' @examples
 #' data(hitter)
-#' data <- varyres(hitter)
+#' data <- with(hitter, varyres(px, pz, hit, 200)
 #' mapit(data[[4]])
 
-varyres <- function(cutoff){
+# devtools::document()
 
-ABCE_List <- list()
+varyres <- function(px, pz, hit, cutoff){
+
+info_list <- list()
 iter <- 0
 fatalities <- numeric()
 
-ABCE <- with(hitter,
+info <- with(hitter,
              cbind.data.frame(
                px = (max(px)+min(px))/2,       # center
                pz = (max(pz)+min(pz))/2,       # center
-               Hitting = round(mean(hit), 3),  # mean
-               Count = dim(hitter)[1],         # obs
+               hitting = round(mean(hit), 3),  # mean
+               count = dim(hitter)[1],         # obs
                width = max(px) - min(px),
                height = max(pz) - min(pz),
                xlb = min(px), xub = max(px),
@@ -26,31 +32,31 @@ ABCE <- with(hitter,
                )
              )
 
-ABCE_List[[iter + 1]] <- ABCE
+info_list[[iter + 1]] <- info
 
-while(sum(ABCE$Count > cutoff) > 0) {
+while(sum(info$count > cutoff) > 0) {
 
   iter <- iter + 1             # Count "while" loops (iterations)
   counter <- 0                 # Count box fatalities
-  LoopData <- data.frame()     # Record within-loop data
+  loop_data <- data.frame()     # Record within-loop data
 
-  for(r in 1:dim(ABCE)[1]){    # (r)ows of ABCE
+  for(r in 1:dim(info)[1]){    # (r)ows of ABCE
 
-    if(ABCE$Count[r] > cutoff){
+    if(info$count[r] > cutoff){
 
       counter <- counter + 1
 
       # Box to subdivide
-      Box_r <- with(ABCE, dplyr::filter(hitter,
+      box_r <- with(info, dplyr::filter(hitter,
                                  px >= xlb[r] & px <= xub[r],
                                  pz >= ylb[r] & pz <= yub[r]))
 
       # x/y box centers for as.image
-      xbc <- with(ABCE, seq(xlb[r], xub[r], , 5)[c(2,4)]) # x
-      ybc <- with(ABCE, seq(ylb[r], yub[r], , 5)[c(2,4)]) # y
+      xbc <- with(info, seq(xlb[r], xub[r], , 5)[c(2,4)]) # x
+      ybc <- with(info, seq(ylb[r], yub[r], , 5)[c(2,4)]) # y
 
       # as.image(...), for p_box
-      gridder_r <- with(Box_r,
+      gridder_r <- with(box_r,
                         fields::as.image(hit,
                                  cbind.data.frame(px, pz),
                                  nx = 2, ny =2,
@@ -59,35 +65,35 @@ while(sum(ABCE$Count > cutoff) > 0) {
                         )
 
       # Organize, save box data to add back
-      ABCE_Box_r <- with(gridder_r, cbind(expand.grid(px = x, pz = y),
-                                          Hitting = as.vector(z),
-                                          Count = as.vector(weights),
-                                          width = rep(ABCE$width[r]/2, 4),
-                                          height = rep(ABCE$height[r]/2, 4)
+      box_r_info <- with(gridder_r, cbind(expand.grid(px = x, pz = y),
+                                          hitting = as.vector(z),
+                                          count = as.vector(weights),
+                                          width = rep(info$width[r]/2, 4),
+                                          height = rep(info$height[r]/2, 4)
                                           )
                          )
 
       # Add x/y lower/upper bounds for next round
-      ABCE_Box_r <- dplyr::mutate(ABCE_Box_r,
+      box_r_info <- dplyr::mutate(box_r_info,
                            xlb = px - width/2,
                            xub = px + width/2,
                            ylb = pz - height/2,
                            yub = pz + height/2
                            )
 
-      LoopData <- rbind.data.frame(LoopData, ABCE_Box_r)
+      loop_data <- rbind.data.frame(loop_data, box_r_info)
 
 
       } # ** END "if" STATEMENT **
     }   # ** END "for" LOOP     **
 
-  ABCE <- rbind.data.frame(dplyr::filter(ABCE, Count <= cutoff), LoopData)
+  info <- rbind.data.frame(dplyr::filter(info, count <= cutoff), loop_data)
 
-  ABCE_List[[iter + 1]] <- ABCE
+  info_list[[iter + 1]] <- info
   fatalities[iter] <- counter
 
   } # ** END "while" LOOP **
 
-ABCE_List[[iter+2]] <- fatalities
-return(ABCE_List)
+info_list[[iter+2]] <- fatalities
+return(info_list)
 }
